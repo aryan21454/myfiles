53d43759d83f34d1e403b615710cc27277a474a6
aryan21454 <108568470+aryan21454@users.noreply.github.com>
2022-12-21 20:18:23 +0530

    Update syscallmodule.c

--- aryan/syscallmodule.c
+++ aryan/syscallmodule.c
@@ -1,9 +1,27 @@
-#include<linux/kernel.h>
-#include<linux/syscalls.h>
-#include<module.h>
-
-
-#define __NR_details_print 287
+#include <linux/module.h>     /* Needed by all modules */
+#include <linux/kernel.h>
+#include<linux/syscalls.h>/* Needed for KERN_INFO */
+#include <linux/init.h>       /* Needed for the macros */
+  
+///< The license type -- this affects runtime behavior
+MODULE_LICENSE("GPL");
+  
+///< The author -- visible when you use modinfo
+MODULE_AUTHOR("Aryan Sharma");
+  
+///< The description -- see modinfo
+MODULE_DESCRIPTION("my modle");
+  
+///< The version of the module
+MODULE_VERSION("0.1");
+  
+static int __init detail_print(void)
+{
+    printk(KERN_INFO "Loading hello module...\n");
+    printk(KERN_INFO "Hello world\n");
+    return 0;
+}
+ #define __NR_detail_print 452
 SYSCALL_DEFINE1(detail_print, pid_t, pid)
 {
   struct task_struct *task;
@@ -19,3 +37,10 @@ SYSCALL_DEFINE1(detail_print, pid_t, pid)
 
   return 0;
 }
+static void __exit detail_print(void)
+{
+    printk(KERN_INFO "Goodbye Mr.\n");
+}
+
+
+
